1bd9429dbbaf3e5dd12a7914ce64fb3358c449e8
Talha <abutalha4323@gmail.com>
2025-10-05 13:04:07 -0400

    Fix Navigate import error

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { ThemeProvider } from './context/ThemeContext';
 import { AuthProvider } from './context/AuthContext';
 import ProtectedRoute from './components/ProtectedRoute';
